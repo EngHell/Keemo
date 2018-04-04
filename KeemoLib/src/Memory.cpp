380b2e3b844d8da63810477779e66958d7bc5519
Angel Recinos <migueln6@gmail.com>
2018-04-04 17:44:21 -0600

    defined writeUInt8 that passes Unit Test

--- KeemoLib/src/Memory.cpp
+++ KeemoLib/src/Memory.cpp
@@ -63,3 +63,49 @@ uint8_t KeemoLib::memory::readUInt8(const uint16_t address)
 	
 	return 0;
 }
+
+void KeemoLib::memory::writeUInt8(const uint16_t address, const uint8_t value)
+{
+	using namespace KeemoLib::memory;
+
+	if (address < ROM_LIMIT)
+	{
+		 rom[address] = value;
+	}
+	else if (address < VRAM_LIMIT)
+	{
+		 vram[address - ROM_LIMIT] = value;
+	}
+	else if (address < SRAM_LIMIT)
+	{
+		 sram[address - VRAM_LIMIT] = value;
+	}
+	else if (address < IRAM_LIMIT)
+	{
+		 iram[address - SRAM_LIMIT] = value;
+	}
+	else if (address < ECHO_IRAM_LIMIT)
+	{
+		 iram[address - IRAM_LIMIT] = value;
+	}
+	else if (address < OAM_LIMIT)
+	{
+		 oam[address - ECHO_IRAM_LIMIT] = value;
+	}
+	else if (address < EMPTY1_LIMIT)
+	{
+		 empty1[address - OAM_LIMIT] = value;
+	}
+	else if (address < IO_LIMIT)
+	{
+		 io[address - EMPTY1_LIMIT] = value;
+	}
+	else if (address < EMPTY2_LIMIT)
+	{
+		 empty2[address - IO_LIMIT] = value;
+	}
+	else if (address < HRAM_LIMIT)
+	{
+		 hram[address - EMPTY2_LIMIT] = value;
+	}
+}
