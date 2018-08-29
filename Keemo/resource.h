b2512a88c7ef4dce5730383152a59b8511423a5c
Angel Recinos <migueln6@gmail.com>
2018-08-29 02:56:53 -0600

    im going to use qt, way too easier

--- Keemo/resource.h
+++ Keemo/resource.h
@@ -11,6 +11,8 @@
 #define IDS_MYAPP_DEBUG_CLASS           104
 #define IDR_MENU1                       107
 #define IDM_MYAPP_MENU_MAIN             108
+#define IDR_TOOLBAR1                    109
+#define IDB_PNG1                        112
 #define ID_FILE_OPEN                    40002
 #define ID_FILE_QUIT                    40003
 #define ID_EMULATION_START              40004
@@ -18,13 +20,14 @@
 #define ID_EMULATION_STOP               40006
 #define ID_EMULATION_RESTART            40007
 #define ID_DEBUG_SHOWDEBUGWINDOW        40008
+#define ID_DEBUG_PLAY                   40011
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        109
-#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40012
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
