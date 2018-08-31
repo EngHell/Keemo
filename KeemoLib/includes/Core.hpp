5e41a1a9e6c3e090c359537960f09289e0936f89
Angel Recinos <migueln6@gmail.com>
2018-08-31 16:54:27 -0600

    added Core.hpp

---
+++ KeemoLib/includes/Core.hpp
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <string>
+
+namespace KeemoLib
+{
+	namespace Core
+	{
+		bool bootUp(std::string path);
+	}
+}
---
+++ KeemoLib/src/Core.cpp
@@ -0,0 +1,27 @@
+﻿#include "../includes/Core.hpp"
+#include "../includes/Cartridge.hpp"
+#include "../includes/Cpu.hpp"
+
+
+bool KeemoLib::Core::bootUp(std::string path)
+{
+	bool sucess = false;
+
+	if(cartridge::loadCartridge(path))
+	{
+		cpu::registers.a = 0x01;
+		cpu::registers.f = 0xb0;
+		cpu::registers.bc = 0x13;
+		cpu::registers.de = 0xd8;
+		cpu::registers.hl = 0x14d;
+
+		cpu::registers.sp = 0xffff;
+		cpu::registers.pc = 0x100;
+
+		// TODO: add the initialization for timers, screen, etc etc when they re ready
+
+		sucess = true;
+	}
+
+	return sucess;
+}
