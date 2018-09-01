2283a6ffbad8ce3b2834274dbb92d5b9e1e587fd
Angel Recinos <migueln6@gmail.com>
2018-08-31 19:49:24 -0600

    fixed endianness of the structs

--- KeemoLib/includes/Cpu.hpp
+++ KeemoLib/includes/Cpu.hpp
@@ -21,8 +21,8 @@ namespace KeemoLib
 				{
 					struct
 					{
-						uint8_t a;
 						uint8_t f;
+						uint8_t a;
 					};
 					uint16_t af;
 				};
@@ -34,8 +34,8 @@ namespace KeemoLib
 				{
 					struct
 					{
-						uint8_t b;
 						uint8_t c;
+						uint8_t b;
 					};
 					uint16_t bc;
 				};
@@ -47,8 +47,8 @@ namespace KeemoLib
 				{
 					struct
 					{
-						uint8_t d;
 						uint8_t e;
+						uint8_t d;
 					};
 					uint16_t de;
 				};
@@ -60,8 +60,8 @@ namespace KeemoLib
 				{
 					struct
 					{
-						uint8_t h;
 						uint8_t l;
+						uint8_t h;
 					};
 
 					uint16_t hl;
