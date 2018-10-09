61b35ea4b5c7c53ba9aec85b234e16e1af4d032b
Unknown <migueln6@gmail.com>
2018-10-09 17:00:11 -0600

    some copy move etc etc constructors for the sprite class

--- KeemoLib/src/video/Sprite.cpp
+++ KeemoLib/src/video/Sprite.cpp
@@ -1,16 +1,63 @@
 #include "Sprite.hpp"
 
-Sprite::Sprite()
+Sprite::Sprite(uint8_t ** rows)
 {
-	rows = new uint8_t * [8];
-
-	for(auto row = 0; row < 8; row++)
+	if(rows)
 	{
-		rows[row] = new uint8_t[8];
+		this->rows = rows;
+	} else
+	{
+		this->rows = new uint8_t * [8];
+
+		for(auto row = 0; row < 8; row++)
+		{
+			this->rows[row] = new uint8_t[8];
+		}
 	}
 }
 
+Sprite::Sprite(const Sprite& other)
+	:Sprite()
+{
+	copy_rows(other.rows);
+}
+
+Sprite::Sprite(Sprite&& other)
+	:Sprite(other.rows)
+{
+	other.rows = nullptr;
+}
+
+Sprite& Sprite::operator=(const Sprite& other)
+{
+	if(this != &other)
+	{
+		copy_rows(other.rows);
+	}
+
+	return *this;
+}
+
+Sprite& Sprite::operator=(Sprite&& other)
+{
+	if(this!=&other)
+	{
+		free();
+
+		rows = other.rows;
+
+		other.rows = nullptr;
+	}
+
+	return *this;
+}
+
 Sprite::~Sprite()
+{
+	free();
+}
+
+void Sprite::free()
 {
 	for(auto row = 0; row < 8; row++)
 	{
@@ -18,4 +65,15 @@ Sprite::~Sprite()
 	}
 
 	delete rows;
+}
+
+void Sprite::copy_rows(uint8_t** rows)
+{
+	for(auto row = 0; row < 8; row++)
+	{
+		for(auto col = 0; col < 8; col++)
+		{
+			this->rows[row][col] = rows[row][col];
+		}
+	}
 }
--- KeemoLib/src/video/Sprite.hpp
+++ KeemoLib/src/video/Sprite.hpp
@@ -5,7 +5,19 @@ class Sprite
 {
 public:
 	uint8_t **rows;
-	Sprite();
+	Sprite(uint8_t ** rows = nullptr);
+	// copy constructor
+	Sprite(const Sprite &sprite);
+	// move constructor
+	Sprite(Sprite &&other);
+	//copy assignment operator
+	Sprite& operator=(const Sprite& other);
+	// move assignment operator
+	Sprite& operator=(Sprite&& other);
 	~Sprite();
+
+private:
+	void free();
+	void copy_rows(uint8_t** rows);
 };
 
