d51e9bd4a0f2fe4ae75e5d79bf0c8ef1bef46ff3
angel <migueln6@gmail.com>
2021-08-31 00:19:03 -0600

    kinda working google test setup

---
+++ KeemoTests/tests/hello_test.cxx
@@ -0,0 +1,13 @@
+//
+// Created by angel on 30/08/21.
+//
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
